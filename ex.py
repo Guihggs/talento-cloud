a2840e5b2df54e1da702381df4ea2e01b252ff7d
guilherme-alves <98426330+Guihggs@users.noreply.github.com>
2023-11-10 17:16:52 -0300

    Add files via upload

---
+++ ex.py
@@ -0,0 +1,12 @@
+# Usando o loop 'for'
+for andar in range(20, 0, -1):
+    if andar != 13:
+        print(andar)
+
+
+# Usando o loop 'while'
+andar = 20
+while andar >= 1:
+    if andar != 13:
+        print(andar)
+    andar -= 1
