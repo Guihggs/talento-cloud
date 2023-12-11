23232aed0b03c2039e594712dc2798ac6f665f73
guilherme-alves <98426330+Guihggs@users.noreply.github.com>
2023-12-11 16:47:11 -0300

    feito

---
+++ preparacaohtml04.py
@@ -0,0 +1,6 @@
+
+lista_produtos = ['máscaras faciais', 'batons', 'esmaltes', 'perfumes', 'loções', 'xampus', 'sabonetes', 'delineadores'] 
+
+lista_produtos[1], lista_produtos[4] = 'rímel', 'cremes hidratantes'
+lista_produtos.append('contonete')
+print(lista_produtos)
