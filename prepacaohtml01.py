ba538c74715d0e3b40c61ed1d40ff23fdcddb7fd
guilherme-alves <98426330+Guihggs@users.noreply.github.com>
2023-12-11 15:58:40 -0300

    feito

---
+++ prepacaohtml01.py
@@ -0,0 +1,9 @@
+# Array com produtos de uma loja
+produtos_loja = ["Maçã", "Notebook", "Cimento", "Guitarra", "Livro"]
+
+# Array com anos de nascimento de familiares e amigos
+anos_nascimento = [1990, 1985, 1978, 2000, 1995]
+
+# Imprimindo os arrays no terminal
+print("Produtos da loja:", produtos_loja)
+print("Anos de nascimento:", anos_nascimento)
